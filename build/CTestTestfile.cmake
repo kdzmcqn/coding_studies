86a88814bd6ad6c65fe76d8eeae4bc24547ef3fa
Leandro Lapeña <lapenaleandro@gmail.com>
2020-09-29 15:36:48 +0800

    further file management

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Nerissa/Documents/vscode/cpp_projects
-# Build directory: C:/Users/Nerissa/Documents/vscode/cpp_projects/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googletest")
-subdirs("tests")
